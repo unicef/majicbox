c1c22b06bc7c1786491a6853918f30ac5568b0fa
mikefab <mikefabrikant@gmail.com>
2016-02-26 00:25:27 -0500

    eslint corrections

--- server.js
+++ server.js
@@ -4,8 +4,8 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');        // call express
+var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
