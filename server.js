17db763f0c1bfe37ed9e63ba6f1534d7dc274612
mikefab <mikefabrikant@gmail.com>
2016-02-26 00:30:13 -0500

    Reset port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8000;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
