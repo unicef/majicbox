03d632e672121203682424b34c8c56893e95777a
john li <jetpack@google.com>
2016-02-27 21:53:44 -0500

    Make config.js lint cleanly.

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 // Note: this file is in .gitignore.
 
 var process_env_or = function(name, default_value) {
-  var process_env = process.env[name];
-  return Boolean(process_env) ? process_env : default_value;
+  return process.env[name] || default_value;
 };
 
 module.exports = {
-  'database':'mongodb://localhost/majicbox',
-  'testdb': process_env_or('TEST_DB', 'mongodb://localhost/test')
+  database: 'mongodb://localhost/majicbox',
+  testdb: process_env_or('TEST_DB', 'mongodb://localhost/test')
 };
