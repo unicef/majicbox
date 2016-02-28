2fc3f67486a126415f3bb1e2f3e3b73bd84070f6
mikefab <mikefabrikant@gmail.com>
2016-02-28 10:20:22 -0500

    Make config names more uniform.

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ var process_env_or = function(name, default_value) {
 };
 
 module.exports = {
-  database: 'mongodb://localhost/majicbox',
-  testdb: process_env_or('TEST_DB', 'mongodb://localhost/test')
+  db_dev: process_env_or('TEST_DB', 'mongodb://localhost/majicbox'),
+  db_test: process_env_or('TEST_DB', 'mongodb://localhost/test')
 };
