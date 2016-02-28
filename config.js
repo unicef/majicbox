6dc89d4b442bdd4851ecdfdb4f11af11f0f616e9
john li <jetpack@google.com>
2016-02-28 15:10:46 -0500

    Extend process_env_or to take array of env vars, tried in sequence.

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
 // Note: this file is in .gitignore.
 
-var process_env_or = function(name, default_value) {
-  return process.env[name] || default_value;
+var _ = require('lodash');
+
+var process_env_or = function(names, default_value) {
+  if (names instanceof Array) {
+    var env_var = _.find(names, function(name) { return process.env[name]; });
+    return process.env[env_var] || default_value;
+  }
+  return process.env[names] || default_value;
 };
 
 module.exports = {
-  database: 'mongodb://localhost/majicbox',
+  port: process_env_or('PORT', 8000),
+  database: process_env_or(['PROD_DB', 'MONGOHQ_URL'],
+                           'mongodb://localhost/majicbox'),
   testdb: process_env_or('TEST_DB', 'mongodb://localhost/test')
 };
