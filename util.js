31f56d83b4cf107e2e9229f9eff59bfa7f944e23
john li <jetpack@google.com>
2016-02-29 23:01:43 -0500

    More reordering.

--- util.js
+++ util.js
@@ -56,6 +56,44 @@ function get_date_condition(model, conditions, start_time, end_time) {
   }
 }
 
+/**
+ * Returns egress mobility data for a region. This indicates movement out of a
+ * region and into other regions.
+ *
+ * @param{string} country_code - Country.
+ * @param{string} origin_region_code - Origin region.
+ * @param{Date} start_time - Date. See comment for get_region_populations.
+ * @param{Date} end_time - Date. See comment for get_region_populations.
+ * @return{Promise} A mapping from date to destination region code to count
+ *   value. Dates are in ISO string format. Counts represent movement from the
+ *   origin region to each destination region. The origin and destination
+ *   regions can be the same, indicating staying in the same region. Example:
+ *   {'2016-02-28T00:00:00.000Z': {'br1': 123, 'br2': 256},
+ *    '2016-02-29T00:00:00.000Z': {'br1': 128, 'br2': 512}}
+ */
+function get_egress_mobility(country_code, origin_region_code, start_time,
+                             end_time) {
+  var conditions = {country_code: country_code,
+                    origin_region_code: origin_region_code};
+  return get_date_condition(Mobility, conditions, start_time, end_time)
+    .then(function(date_condition) {
+      if (!date_condition) { return {}; }
+      return new Promise(function(res, rej) {
+        conditions.date = date_condition;
+        Mobility.find(conditions).exec(function(err, docs) {
+          if (err) { return rej(err); }
+          var result = {};
+          docs.forEach(function(mobility) {
+            _.set(result, [mobility.date.toISOString(),
+                           mobility.destination_region_code],
+                  mobility.count);
+          });
+          res(result);
+        });
+      });
+    });
+}
+
 /** Returns relative population estimates for the country.
  * @param{string} country_code - Country.
  * @param{Date} start_time - Date. See comment for end_time.
@@ -97,44 +135,6 @@ function get_region_populations(country_code, start_time, end_time) {
       });
       return Promise.all(region_promises).then(function() { return result; });
     }));
-}
-
-/**
- * Returns egress mobility data for a region. This indicates movement out of a
- * region and into other regions.
- *
- * @param{string} country_code - Country.
- * @param{string} origin_region_code - Origin region.
- * @param{Date} start_time - Date. See comment for get_region_populations.
- * @param{Date} end_time - Date. See comment for get_region_populations.
- * @return{Promise} A mapping from date to destination region code to count
- *   value. Dates are in ISO string format. Counts represent movement from the
- *   origin region to each destination region. The origin and destination
- *   regions can be the same, indicating staying in the same region. Example:
- *   {'2016-02-28T00:00:00.000Z': {'br1': 123, 'br2': 256},
- *    '2016-02-29T00:00:00.000Z': {'br1': 128, 'br2': 512}}
- */
-function get_egress_mobility(country_code, origin_region_code, start_time,
-                             end_time) {
-  var conditions = {country_code: country_code,
-                    origin_region_code: origin_region_code};
-  return get_date_condition(Mobility, conditions, start_time, end_time)
-    .then(function(date_condition) {
-      if (!date_condition) { return {}; }
-      return new Promise(function(res, rej) {
-        conditions.date = date_condition;
-        Mobility.find(conditions).exec(function(err, docs) {
-          if (err) { return rej(err); }
-          var result = {};
-          docs.forEach(function(mobility) {
-            _.set(result, [mobility.date.toISOString(),
-                           mobility.destination_region_code],
-                  mobility.count);
-          });
-          res(result);
-        });
-      });
-    });
 }
 
 /** Simple timing tool. `stopwatch.reset` resets the timer. Subsequent calls to
