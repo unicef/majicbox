c131be41f73c57d5a6de718d064670dc0327dc91
mikefab <mikefabrikant@gmail.com>
2017-02-10 18:33:28 -0500

    Move travel activity aggregation to helper

--- util.js
+++ util.js
@@ -7,10 +7,6 @@ var Weather = require('./app/models/weather');
 var request = require('superagent');
 var azure = require('./lib/azure_storage');
 var bluebird = require('bluebird');
-var moment = require('moment');
-
-// For google geochart in magicbox dashboard app
-var country_code_3_to_2 = JSON.parse(fs.readFileSync('./lib/country_codes.json', 'utf8'));
 
 function remove_file(path, file) {
   return new Promise(function(resolve, reject) {
@@ -351,63 +347,6 @@ function summary_mobility() {
 }
 
 /**
- * Returns aggregated activity by country.
- *
- * @param{Date} start_date - See comment near the top of this module.
- * @param{Date} end_date - See comment near the top of this module.
- * @param{string} origin_country_code - Origin country.
- * @return{Promise} Aggregate country departure activty by date.
-  Example:
- * {
- *   "origin_country_code": "VI",
- *   "count": 17869
- * },
- */
-function travel_from_country_activity(start_date, end_date, origin_country_code) {
-  start_date = moment(parseInt(start_date, 10)).toDate();
-  end_date = moment(parseInt(end_date, 10)).toDate();
-
-  var obj_match = {
-    date: {$gte: start_date, $lte: end_date}
-  };
-
-  if (origin_country_code) {
-    obj_match = Object.assign(
-      {origin_country_code: origin_country_code.toUpperCase()},
-      obj_match
-    );
-  }
-
-  return new Promise(function(resolve, reject) {
-    Mobility.aggregate(
-      [
-        {$match: obj_match
-        },
-        {
-          $group:
-          {
-            _id: {origin_country_code: "$origin_country_code"},
-            count: {$sum: "$count"}
-          }
-        }
-      ]
-    ).exec(function(err, doc) {
-      if (err) {
-        return reject(err);
-      }
-      resolve(
-        doc.map(function(obj) {
-          return {
-            origin_country_code: country_code_3_to_2[obj._id.origin_country_code],
-            count: obj.count
-          };
-        })
-      );
-    });
-  });
-}
-
-/**
  * Returns relative population estimates for the country based on mobility data.
  *
  * @param{string} country_code - Country.
@@ -487,7 +426,6 @@ var stopwatch = (function() {
 
 module.exports = {
   get_amadeus_file_names_already_in_mongo: get_amadeus_file_names_already_in_mongo,
-  travel_from_country_activity: travel_from_country_activity,
   summary_azure: summary_azure,
   summary_amadeus: summary_amadeus,
   summary_mobility: summary_mobility,
