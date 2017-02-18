9b6aedb5b3e56d87257d501554e46f6f538e21c4
mikefab <mikefabrikant@gmail.com>
2017-02-17 21:55:23 -0500

    Activity

--- app/handlers/index.js
+++ app/handlers/index.js
@@ -7,9 +7,11 @@ import weather from './weather'
 import mobility from './mobility'
 import admins from './admins'
 import summeries from './summeries'
+import activity from './activity'
 
 export default express.Router()
   .use(weather)
   .use(mobility)
   .use(admins)
   .use(summeries)
+  .use(activity)
